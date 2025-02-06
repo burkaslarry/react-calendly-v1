92c5c8ae5b4a9717cb7ca0c8beb51c300321440a
Larry Lo <gaogaigargalden@gmail.com>
2025-02-06 19:03:29 +0800

    feat: calendar

---
+++ src/app/component/CalendlyWidget.tsx
@@ -0,0 +1,11 @@
+'use client'; // Required for the app directory
+
+import { InlineWidget } from "react-calendly";
+
+const CalendlyWidget = () => {
+  return (
+    <InlineWidget url="https://calendly.com/larrylo1107/30min" />
+  );
+};
+
+export default CalendlyWidget;
---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import CalendlyWidget from "./component/CalendlyWidget";
+
+
+const Home = () => {
+  return (
+    <div className="App">
+      <CalendlyWidget />
+    </div>
+  );
+};
+
+export default Home;
